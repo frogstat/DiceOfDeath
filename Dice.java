609cb9ec91ca0b7ffe4b70b802c3549a24b41cf0
frogstat <frogstatftw@gmail.com>
2025-11-03 09:02:48 +0100

    Add Dice.java

---
+++ Dice.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Dice {
+
+    private int sidesOfDice;
+
+    public Dice(int sidesOfDice){
+        this.sidesOfDice = sidesOfDice;
+    }
+
+    public Dice(){
+        this.sidesOfDice = 6;
+    }
+
+    public int getSidesOfDice(){
+        return sidesOfDice;
+    }
+
+    public int rollDice(){
+        Random random = new Random();
+        return random.nextInt(sidesOfDice + 1);
+    }
+
+}
