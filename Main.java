3883ad70a2c0ce0979915c106e11202228d505a1
frogstat <frogstatftw@gmail.com>
2025-11-03 09:07:11 +0100

    add Main

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Dice dice = new Dice();
+
+        System.out.println("Welcome to Dice of Death!");
+        System.out.println("**************************");
+        System.out.println(dice.rollDice());
+        System.out.println(dice.rollDice());
+        System.out.println(dice.rollDice());
+        System.out.println(dice.rollDice());
+        System.out.println(dice.rollDice());
+
+    }
+}
