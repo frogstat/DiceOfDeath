d1ea0cac36a79d03c1079ed2553a1512fa37166a
Felix Andersson <felix.andersson@gmail.com>
2025-11-03 09:31:15 +0100

    genrated getters and setters

--- Player.java
+++ Player.java
@@ -10,11 +10,61 @@ public class Player {
 
     }
 
+
+
     public void takeDamage(int dmg) {
         this.health -= dmg;
     }
 
     public boolean isAlive() {
         return this.health > 0;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public int getHealth() {
+        return health;
+    }
+
+
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+
+
+    public int getAttack() {
+        return attack;
+    }
+
+
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+
+
+    public int getDefense() {
+        return defense;
+    }
+
+
+
+    public void setDefense(int defense) {
+        this.defense = defense;
     }
 }
