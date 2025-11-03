762835722a259ffae8c8cc4a3dc9c79f9094b602
frogstat <frogstatftw@gmail.com>
2025-11-03 09:52:03 +0100

    add damage

--- GameClass.java
+++ GameClass.java
@@ -7,10 +7,26 @@ public class GameClass {
         this.dice = dice;
     }
 
-    public int playRound(Player player1, Player player2){
+    public int getWinner(Player player1, Player player2){
         int resultPlayer1 = dice.rollDice();
+        System.out.println(player1.getName() + " rolled " + resultPlayer1);
         int resultPlayer2 = dice.rollDice();
+        System.out.println(player2.getName() + " rolled " + resultPlayer2);
         return resultPlayer1 - resultPlayer2; // negative = player 2 wins.
+    }
+
+    public void playRound(Player player1, Player player2){
+
+        int result = getWinner(player1, player2);
+        if(result > 0){
+            player2.takeDamage(result* player1.getAttack()*player2.getDefense());
+        }
+        else if(result<0){
+            player1.takeDamage((result*-1)* player2.getAttack()*player1.getDefense());
+        }
+        else if(result == 0){
+            System.out.println("It's a tie!");
+        }
     }
 
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Dice dice = new Dice();
+        GameClass diceOfDeath = new GameClass(dice);
 
         Player player1 = new Player("Felix", 100, 0,0);
         Player player2 = new Player("Eric", 100, 0,0);
@@ -16,7 +17,7 @@ public class Main {
 
         while(player1.getHealth() > 0 || player2.getHealth() > 0){
 
-
+            diceOfDeath.playRound(player1, player2);
 
 
 
